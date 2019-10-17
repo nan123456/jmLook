8dde5090f2a632f89afc1bb346bae3198969a4da
guojinyuan <gjyguojinyuan@outlook.com>
2019-10-17 22:44:11 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
     proxyTable: {
       '/api':{
         target:'http://jsonplaceholder.typicode.com',
@@ -65,7 +65,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
